9ff28d5cd8481b9e210e13a2b7d4bcb26ce07f7a
marcinp <marcin.pienkowski@e3code.pl>
2020-12-29 14:06:05 +0100

    Fix

--- example/main.go
+++ example/main.go
@@ -12,29 +12,18 @@ import (
 
 func HandlerSIGUSR1() {
 	fmt.Println("SIGUSR1 received")
-
 }
 
 func main() {
-
 	fmt.Printf("Test from console with:\nkill -SIGUSR1 %v\n", os.Getpid())
-
-	// Add signal handler
 	gossig.SignalProcessor.HandlerAdd(syscall.SIGUSR1, HandlerSIGUSR1)
-
 	fmt.Println("Start processing")
 	gossig.SignalProcessor.Run()
-
 	time.Sleep(time.Second * 20)
-
 	fmt.Println("Stop processing")
 	gossig.SignalProcessor.Stop()
-
 	time.Sleep(time.Second * 5)
-
 	fmt.Println("Restart processing")
 	gossig.SignalProcessor.Run()
-
 	time.Sleep(time.Second * 5)
-
 }
